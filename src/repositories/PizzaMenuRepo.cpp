4f6b11ddd140a6ae6a71d9b12da71d22f84cb786
kristinbh <34029530+kristinbh@users.noreply.github.com>
2017-12-12 14:49:04 +0000

    d

--- src/repositories/PizzaMenuRepo.cpp
+++ src/repositories/PizzaMenuRepo.cpp
@@ -14,42 +14,24 @@ void PizzaMenuRepo::write(PizzaMenu& menu){
     fout.close();
 }
 
-
-
-void PizzaMenuRepo::read_in_vector(string line){
-    string str = "";
-    vector<string> pizza;
-    for (unsigned int i = 0; i < line.length(); i++) {
-        if (line[i] == ','){
-             pizza.push_back(str);
-            str = "";
-        }
-        else {
-            str += line[i];
-        }
-
-    }
-    for(size_t i = 0; i < pizza.size(); i++){
-        cout << pizza[i] << endl;
-
-    }
-
-}
-
 void PizzaMenuRepo::read(){
 
     ifstream fin;
     string str;
     fin.open("Pizza-Menu.txt");
+    vector<string> pizza;
 
     if (fin.is_open()){
             while (getline(fin, str)){
-                    read_in_vector(str);
-
+                    pizza.push_back(str);
+                    str = "";
             }
-
-        fin.close();
+            for(size_t i = 0; i < pizza.size(); i++){
+        cout << pizza[i] << endl;
+         fin.close();
+        }
     }
+
     else{
 
         cout << "can not write in file, file is closed" << endl;
